7f6fd56ebe0d52bdd20e02b93247559f0276d348
Chen Tianao <sc19t2c@leeds.ac.uk>
2021-03-28 15:23:49 +0800

    Last version forget to upload the borrow_book.c

--- load_book.c
+++ load_book.c
@@ -1,39 +1,58 @@
-//这时last 将会变为头结点 
-void readbook(struct Node* lastx){
-	struct Node* newNode;
-	struct Node* last;
-	int i = 0;
-	char buf[300];
-	last = lastx;     
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"book_management.h"
+extern BookArray node; 
+extern int kindbook;
+extern int line;
+int load_books(FILE *file){
+	Book* newNode = (Book*)malloc(sizeof(Book));
+	Book* last = (Book*)malloc(sizeof(Book));
+	char buf[300];  //to store the each line's information  
+	int n = 0;     //used to judge loop if is ended  
+	last = node.array;     
 	FILE *fp;
-	fp = fopen("book.txt","r+");
+	fp = fopen(file,"rt");
 	if(fp!=NULL)
-	{
-		while(i<kindbook)
+	{	
+		fgets(buf,200,fp);
+		sscanf(buf,"The total amount of the book is %d\n",&node.length);
+		fgets(buf,200,fp);
+		sscanf(buf,"The kinds of book are %d\n",&kindbook);
+		fgets(buf,200,fp);
+		sscanf(buf,"The following lines are %d\n",&line);
+		while(n<line)
 		{	
-			fgets(buf, 300, fp);
-			sscanf(buf,"ID:%d\n", &newNode->data.id);
-			fgets(buf, 300, fp);
-			sscanf(buf, "Book Title:\%[^\n]",newNode->data.title);
-			fgets(buf, 300, fp);
-			sscanf(buf,"Author(s):\%[^\n]",newNode->data.authors);
-			fgets(buf, 300, fp);
-			sscanf(buf,"Year:%d\n", &newNode->data.year);
-			fgets(buf, 300, fp);
-			sscanf(buf,"Copies:%d\n", &newNode->data.copies);
-			fgets(buf, 300, fp);
-			sscanf(buf,"Borrow_Copies:%d\n", &newNode->data.brcopies);
-			newNode = (struct Node*)malloc(sizeof(struct Node)); //创立结点，以便接受读取的信息 
-      		newNode->next = last;
-			last->next = NULL;
-			i++;
+			newNode = (Book*)malloc(sizeof(Book)); //Creates a node to accept information loaded 
+			newNode->title = (char*)malloc(sizeof(char));
+			newNode->authors = (char*)malloc(sizeof(char));   //Allocates space for Pointers 
+			fgets(buf, 200, fp);
+			sscanf(buf,"ID:%d\n", &newNode->id);
+			fgets(buf, 200, fp);
+			sscanf(buf, "Book Title:%s\n",newNode->title);
+			fgets(buf, 200, fp);
+			sscanf(buf,"Author(s):%s\n",newNode->authors);
+			fgets(buf, 200, fp);
+			sscanf(buf,"Year:%d\n", &newNode->year);
+			fgets(buf, 200, fp);
+			sscanf(buf,"Copies:%d\n", &newNode->copies);
+			fgets(buf, 200, fp);
+			sscanf(buf,"Borrow_Copies:%d\n", &newNode->brcopies);
+      		last->next = newNode;
+			newNode = newNode->next;
+			last = last->next;
+			n += 6;
 		}
+		last->next = NULL;
 		fclose(fp);
+		return 0;
   	}
   	else 
 	{
-   		printf("\t\tFailing to load the book.txt,please check it!\n\n");
+   		printf("\t\tFailing to load the book.txt,\
+maybe it's the first time to use the system!\n\n");
 		getch();
-		coremain();
+		fclose(fp);
+		return -1;
   	}
 }
