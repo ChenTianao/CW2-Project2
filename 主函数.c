ed0703548667b67e334575ba14a77eac2ac57190
Chen Tianao <sc19t2c@leeds.ac.uk>
2021-05-04 15:15:26 +0800

    The fourth version. short distance and the information of path could be output. and visualization of data has been accomplished. However,some error still not be addressed

--- 主函数.c
+++ 主函数.c
@@ -8,22 +8,10 @@ int main(){
 	long int startnode_id;
 	long int endnode_id;
 	read_data();
-	//test1
-	printf("read_data\n");
-	//end
 	convert();
-	//test2
-	printf("convert\n");
-	//end
 	printf("Please print the start node id and end node id:\n");
 	scanf("%ld%ld",&startnode_id,&endnode_id);
 	Gragh(startnode_id);
-	//test3
-	printf("Gragh\n");
-	//end
-	Output(endnode_id);
-	//test4
-	printf("Output\n");
-	//end 
+	Output(startnode_id,endnode_id);
 	return 0;
 } 
--- 求最短路径.c
+++ 求最短路径.c
@@ -6,6 +6,8 @@
 #include"Read.h"
 #include"List.h"
 extern struct adjacency_list sum_list;
+extern Readlink Readlinkarray[1000000];
+extern Readnode Readnodearray[1000000];
 //创造俩玩意儿
 	Ensure ensure_list[NODE];
 	Unsure unsure_list[NODE-1];
@@ -24,12 +26,14 @@ void Gragh(long int nodestart){
 			ensure_list[0].shortrst_distance = 0;
 			ensure_list[0].ensure_nodelat = sum_list.vertex[i].topnode.latitude;
 			ensure_list[0].ensure_nodelon = sum_list.vertex[i].topnode.longitude;
+			ensure_list[0].all_nodeid[0] = nodestart;
 		}
 		else{
 			unsure_list[m].unsure_nodeid = sum_list.vertex[i].topnode.node_id;
 			unsure_list[m].shortrst_distance = 0;
 			unsure_list[m].unsure_nodelat = sum_list.vertex[i].topnode.latitude;
 			unsure_list[m].unsure_nodelon = sum_list.vertex[i].topnode.longitude;
+			unsure_list[m].all_nodeid[0] = nodestart;
 			unsure_list[m].x = 0;
 			m += 1;
 		}
@@ -51,40 +55,60 @@ void Gragh(long int nodestart){
 			updatelast = sum_list.vertex[i].nodenext;
 			//寻找当前顶点 
 			if(ensure_list[s1].ensure_nodeid == sum_list.vertex[i].topnode.node_id){
-				//printf("%ld\n",ensure_list[s1].ensure_nodeid);
+			//	printf("%ld",ensure_list[s1].ensure_nodeid);
+			//	getch();
+			//	printf("%ld\n",ensure_list[s1].ensure_nodeid);
 				//先把不确定集合与该点相连的点的信息更新
 				 while(updatelast != NULL){
+				 	//printf("ttt");
+				//	getch();
 				 	int j;
 				 	for(j = 0;j<NODE-1;j++){
-						if(updatelast->nodelater.node_id == unsure_list[j].unsure_nodeid){
+						if(updatelast->nodelater.node_id == unsure_list[j].unsure_nodeid && unsure_list[j].x != 1){
+						//	printf("%ld",updatelast->nodelater.node_id);
 							//若新路径更小，则更新 
-							if(updatelast->distance + ensure_list[s1].shortrst_distance < unsure_list[j].shortrst_distance || 
+							if((updatelast->distance + ensure_list[s1].shortrst_distance < unsure_list[j].shortrst_distance) || 
 								unsure_list[j].shortrst_distance == 0 )
 				 			{	
+				 			//	printf("gengxin");
 								unsure_list[j].shortrst_distance = updatelast->distance + ensure_list[s1].shortrst_distance;
-								//储存路径经过的点的信息 
-								int t;
-								for(t = 0;t<10000;t++){
-									//循环之前，先清空先前的路径
+							//	printf("%lf",	unsure_list[j].shortrst_distance);
+								//循环之前，先清空先前的路径
 									int pop;
 									for(pop = 0;pop<10000;pop++){
 										unsure_list[j].all_nodeid[pop] = 0;
 									} 
-									//清空结束 
+									//清空结束
+								//储存路径经过的点的信息 
+								int t;
+								for(t = 0;t<10000;t++){
 									if(ensure_list[s1].all_nodeid[t] == 0){
 										unsure_list[j].all_nodeid[t] = unsure_list[j].unsure_nodeid;
+									//	printf("%ld",unsure_list[j].all_nodeid[t]);
+									//	printf("%d",t);
+										//if(ensure_list[s1].ensure_nodeid == 288976339)
+											/*{
+											printf("ok");
+											printf("%ld",ensure_list[s1].ensure_nodeid);
+											getch();
+											}*/
 										break;
+										
 									}
 									else{
 										unsure_list[j].all_nodeid[t] = ensure_list[s1].all_nodeid[t];
+									//	printf("%ld",unsure_list[j].all_nodeid[t]);
+									//	printf("%d",t);
 									}
 								}
+								//	printf("\n%ld\n",unsure_list[j].all_nodeid[0]);
 							}
 				 			break;
 						}
 					}
 					updatelast = updatelast->next;
 				 }
+				//printf("3");
 				//在未确定集合中找寻下一次离原始点最近的点
 				int m;
 				for(m = 0;m<NODE-1;m++){
@@ -99,7 +123,7 @@ void Gragh(long int nodestart){
 						{
 							min_distance = unsure_list[m].shortrst_distance;
 							//test
-							//printf("%lf",min_distance);
+						//	printf("%lf",min_distance);
 							//getch();
 						}
 					}
@@ -107,22 +131,32 @@ void Gragh(long int nodestart){
 				//将已确定顶点放进已确定顶点集合中 
 				int n;
 				for(n = 0;n<NODE-1;n++){
+				//	printf("\nnaru");
 					if(min_distance == unsure_list[n].shortrst_distance && unsure_list[n].x != 1){
+					//	printf("%ld\n\n",unsure_list[n].unsure_nodeid);
+					//	printf("%ld\n\n",unsure_list[n].all_nodeid[0]);
+					//	printf("%d",n);
+						
 						s1 += 1;
 						ensure_list[s1].ensure_nodeid = unsure_list[n].unsure_nodeid;
+						
 						ensure_list[s1].ensure_nodelat = unsure_list[n].unsure_nodelat;
 						ensure_list[s1].ensure_nodelon = unsure_list[n].unsure_nodelon;
 						ensure_list[s1].shortrst_distance = min_distance;
+						//对路径进行赋值
+						int k = 0;
+						while(unsure_list[n].all_nodeid[k] != 0){
+							ensure_list[s1].all_nodeid[k] = unsure_list[n].all_nodeid[k];
+							//printf("ok");
+							k += 1;
+						}
 						unsure_list[n].x = 1;  //对进入已确定集合进行标记
 						break; 
 					}
 				}
 				//printf("kongge\n");
-				
 				break;          //若已经找到了最短路径，就跳出循环 
 			}
-			
-			
 		}
 		/*printf("%ld\n",ensure_list[s1].ensure_nodeid);
 		y++;
@@ -130,12 +164,66 @@ void Gragh(long int nodestart){
 		if(s1==NODE-1)
 			break;                  //所有点都更新完成 
 	}
-	
+
 }
 
-void Output(long int nodeend){
-	
-	
+void Output(long int nodestart,long int nodeend){
+	FILE *fp;
+	FILE *fp1;
+	fp = fopen("all.txt","wt");
+	int i = 0;
+	while(Readlinkarray[i].linkid != 0){
+		int x = 0;
+		int j = 0;
+		for(j = 0;j<100000;j++){
+			if(Readlinkarray[i].node_id1 == Readnodearray[j].nodeid){
+				fprintf(fp,"%lf ",Readnodearray[j].nodelon);
+				fprintf(fp,"%lf\n",Readnodearray[j].nodelat);
+				x++;
+			}
+			if(Readlinkarray[i].node_id2 == Readnodearray[j].nodeid){
+				fprintf(fp,"%lf ",Readnodearray[j].nodelon);
+				fprintf(fp,"%lf\n",Readnodearray[j].nodelat);
+				x++;
+			}
+			
+			if(x == 2){
+				fprintf(fp,"\n");
+				break;
+			}
+		}
+		i++;
+	}
+	fclose(fp);
+	fp1 = fopen("short.txt","wt");
+	int m = 0;
+	for(m=0;m<NODE;m++){
+		if(ensure_list[m].ensure_nodeid == nodeend){
+			int n = 1;
+			while(ensure_list[m].all_nodeid[n] != 0){
+				
+				int x = 0;
+				int j = 0;
+				for(j = 0;j<10000;j++){
+					if(Readnodearray[j].nodeid == ensure_list[m].all_nodeid[n-1]){
+						fprintf(fp,"%lf ",Readnodearray[j].nodelon);
+						fprintf(fp,"%lf\n",Readnodearray[j].nodelat);
+						x++;
+					}
+					if(Readnodearray[j].nodeid == ensure_list[m].all_nodeid[n]){
+						fprintf(fp,"%lf ",Readnodearray[j].nodelon);
+						fprintf(fp,"%lf\n",Readnodearray[j].nodelat);
+						x++;
+					}
+					if(x == 2){
+						fprintf(fp,"\n");
+						break;
+					}
+				}
+				n++;
+			}
+		}
+	}
 	
 	
 }
